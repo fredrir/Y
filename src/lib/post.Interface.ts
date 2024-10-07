696c2d3136497969b1121634adab6495c64d8eab
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-10-07 16:53:38 +0200

    feat: :sparkles: Added post component

---
+++ src/lib/post.Interface.ts
@@ -0,0 +1,11 @@
+export type PostType = {
+  id: string;
+  body: string;
+  author: string;
+  amtLikes: number;
+  amtComments: number;
+};
+
+export type CommentType = PostType & {
+  parentID: string;
+};
